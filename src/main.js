28128ee40dc6d30c6f46c109c85c256bb5ff85bb
Merry  HA <69305320+MerryHa@users.noreply.github.com>
2020-12-18 15:41:28 +0900

    JS : 개선 2
    setEventListeners() 작성+마무리

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 'use strict';
+
+//main
+loadItems()
+.then(items=>{
+    displayItems(items);
+    setEventListeners(items);
+})
+.catch(console.log);
+
 //Fetch the items from the JSON file
 function loadItems(){
     return fetch('../data/data.json')
         .then(response=>response.json())
         .then(json=>json.items);
 }
+
+const container=document.querySelector('.items');
 //Update the list with the given items
-function displayItems(items){
-    const container=document.querySelector('.items');
+function displayItems(items){ 
     container.innerHTML=items.map(item=>createHTMLString(item)).join('');
 }
 //Create HTML list item from the given data item
@@ -19,53 +29,34 @@ function createHTMLString(item){
     </li>
     `;
 }
-//main
-loadItems()
-.then(items=>{
-    displayItems(items);
-    //setEventListeners(items);
-})
-.catch(console.log);
 
-// const items=document.querySelector('.items');
-// function displayItems(jsonItems){
-//     for (let i=0; i < jsonItems.length; i++) {
-//         const item=document.createElement('li');
-//         const json=jsonItems[i];
-//         item.setAttribute('class','item');
-//         item.setAttribute('data-color',json['color']);
-//         item.setAttribute('data-type',json['type']);
-//         item.innerHTML=`<img src="${json['image']}" class="item__thumbnail" alt="${json['type']}">${json['gender']}, ${json['size']}`;
-//         items.append(item);
-//     }
-// }
-
-function setEventListeners(){
-
+function setEventListeners(items){
+    const logo=document.querySelector('.logo');
+    const buttons=document.querySelector('.buttons');
+    logo.addEventListener('click',()=>displayItems(items));
+    buttons.addEventListener('click',event=>onButtonClick(event,items));
 }
-const btns=document.querySelector('.buttons');
-btns.addEventListener('click',(e)=>{
-    const target=e.target;
-    if(target.nodeName!=='IMG'&&target.nodeName!=='BUTTON'){
-        return;
-    }else{
-        filtering(target);
-    }
-})
-function filtering(target){
-    if(target.nodeName==='IMG'){
-        filterItem(target.parentNode.getAttribute('data-type'));
-    }else{
-        filterItem(target.getAttribute('data-color'));
-    }
-}
-function filterItem(info){
-    const itemArray=document.querySelectorAll('.item');    
-    for (let item of itemArray) {
-        if((item.getAttribute('data-type')===info)||(item.getAttribute('data-color')===info)){
-            item.classList.remove('hidden');
+
+
+//Handle button click
+function onButtonClick(event, items){
+     const key=event.target.dataset.key;
+     const value=event.target.dataset.value;
+
+     if(key==null||value==null){
+         return;
+     }
+     updateItems(items,key,value);
+}  
+
+//Make the items matching {key: value} invisible.
+function updateItems(jsonItems,btnKey,btnValue){
+    const items=document.querySelectorAll('.item');
+    items.forEach(item =>{
+        if(item.dataset[btnKey]===btnValue){
+            item.classList.remove('invisible');
         }else{
-            item.classList.add('hidden');
+            item.classList.add('invisible');
         }
-    }
-};
+    });
+}  
