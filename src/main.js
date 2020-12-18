31afa070bc205d6213e297a15f6c2d8653051164
Merry  HA <69305320+MerryHa@users.noreply.github.com>
2020-12-18 11:05:04 +0900

    JS : 개선 1
    displayItems(), createHTMLString()

--- src/main.js
+++ src/main.js
@@ -5,7 +5,20 @@ function loadItems(){
         .then(response=>response.json())
         .then(json=>json.items);
 }
-
+//Update the list with the given items
+function displayItems(items){
+    const container=document.querySelector('.items');
+    container.innerHTML=items.map(item=>createHTMLString(item)).join('');
+}
+//Create HTML list item from the given data item
+function createHTMLString(item){
+    return `
+    <li class="item" data-color="${item.color}" data-type="${item.type}">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail"/>
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
 //main
 loadItems()
 .then(items=>{
@@ -14,18 +27,18 @@ loadItems()
 })
 .catch(console.log);
 
-const items=document.querySelector('.items');
-function displayItems(jsonItems){
-    for (let i=0; i < jsonItems.length; i++) {
-        const item=document.createElement('li');
-        const json=jsonItems[i];
-        item.setAttribute('class','item');
-        item.setAttribute('data-color',json['color']);
-        item.setAttribute('data-type',json['type']);
-        item.innerHTML=`<img src="${json['image']}" class="item__thumbnail" alt="${json['type']}">${json['gender']}, ${json['size']}`;
-        items.append(item);
-    }
-}
+// const items=document.querySelector('.items');
+// function displayItems(jsonItems){
+//     for (let i=0; i < jsonItems.length; i++) {
+//         const item=document.createElement('li');
+//         const json=jsonItems[i];
+//         item.setAttribute('class','item');
+//         item.setAttribute('data-color',json['color']);
+//         item.setAttribute('data-type',json['type']);
+//         item.innerHTML=`<img src="${json['image']}" class="item__thumbnail" alt="${json['type']}">${json['gender']}, ${json['size']}`;
+//         items.append(item);
+//     }
+// }
 
 function setEventListeners(){
 
